2dd4f55f023ada8aca28396e83fd89b2953a8fc7
kwanyoung92 <skycoder@daum.net>
2018-06-03 00:41:03 +0900

    bracket debugging

--- ctci-balanced-brackets/main.cpp
+++ ctci-balanced-brackets/main.cpp
@@ -3,112 +3,72 @@
 using namespace std;
 
 class Stack {
-	vector<char> stack;
-	int top;
-
-	public :
-		Stack(int stack_size) {
-			stack.resize(stack_size);
-			top = -1;
-		}
-		bool is_empty();
-		bool is_full();
-		void push(char data);
-		char pop();
+    vector<char> stack;
+    int top;
+    public :
+        Stack(int stack_size) : top(-1) {
+            stack.resize(stack_size); 
+        }
+        bool is_full();
+        bool is_empty();
+        void push(char data);
+        char pop();
 };
 
-class Queue {
-	vector<char> queue;
-	int head;
-	int tail;
-
-	public :
-		Queue(int queue_size) {
-			queue.resize(queue_size);
-			head = 0;
-			tail = -1;
-		}
-		bool is_empty();
-		bool is_full();
-		void enqueue(char data);
-		char dequeue();
-};
-
-void Stack::push(char data) {
-	if(this->is_full()) {
-		return;
-	}
-
-	++top;
-	stack[top] = data;
-}
-
-char Stack::pop() {
-	if(!this->is_empty()) {
-		char return_value = stack[top];
-		--top;
-		return return_value;
-	}
-	else {
-		cout << "Error: Stack is empty. Can't pop any data." << endl;
-		return '\0';
-	};
+bool Stack::is_full() {
+    if(stack.size() == (top+1))
+        return true;
+    else
+        return false;
 }
 
 bool Stack::is_empty() {
-	if(top == -1) return true;
-	else return false;
+    if(top == -1)
+        return true;
+    else
+        return false;
 }
 
-bool Stack::is_full() {
-	if(stack.size() == top) return true;
-	else return false;
+void Stack::push(char data) {
+    if(this->is_full()) {
+        return; 
+    }
+    else {
+        ++top;
+        stack[top] = data;
+    }
 }
 
-void Queue::enqueue(char data) {
-	if(this->is_full()) { 
-		cout << "Error: Queue is full. Can't enqueue more data." << endl;
-		return;
-	}
-
-	++tail;
-	queue[tail] = data;
+char Stack::pop() {
+    if(this->is_empty()) {
+        return '\0';
+    } 
+    else {
+        char pop_data = stack[top];
+        --top;
+        return pop_data;
+    }
 }
 
-char Queue::dequeue() {
-	if(this->is_empty()) {
-		cout << "Error: Queue is empty. Can't dequeue any data." << endl;
-		return '\0';
-	}
-	
-	char return_value = queue[head];
-	++head;
-	return return_value;
+bool is_open_bracket(char bracket) {
+    switch(bracket) {
+        case '(' : case '[' : case '{' :
+            return true;    
+    }
+    return false;
 }
 
-bool Queue::is_empty() {
-	if(tail == -1) return true;
-	else return false;
+char return_couple_bracket(char bracket) {
+    switch(bracket) {
+        case ')' :
+            return '(';
+        case '}' :
+            return '{';
+        case ']' :
+            return '[';
+    } 
+    return '\0';
 }
-
-bool Queue::is_full() {
-	if(queue.size() == tail) return true;
-	else return false;
-}
-
-char find_couple_bracket(char bracket) {
-	switch(bracket) {
-		case '(' :
-			return ')';
-		case '[' :
-			return ']';
-		case '{' :
-			return '}';
-		default :
-			return '\0';
-	}
-}
-
 
 int main()
 {
@@ -116,40 +76,38 @@ int main()
     cin >> t;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-	vector<bool> result(t);	// save result;
-
+    vector<bool> result; 
+    
     for (int t_itr = 0; t_itr < t; t_itr++) {
         string expression;
         getline(cin, expression);
-
-		// find bracket
-		int str_length = expression.length();
-		Stack s(str_length/2);
-		Queue q(str_length/2);
-
-		for(int i = 0; i < str_length; ++i) {
-			if(i < str_length / 2) {
-				s.push(expression[i]);
-			}
-			else {
-				q.enqueue(expression[i]);
-			}
-		}
-		
-		bool flag = true;
-		for(int i = 0; i < str_length / 2; ++i) {
-			if(find_couple_bracket(s.pop()) != q.dequeue()) {
-				flag = false;
-				break;
-			}
-		}
-		result.push_back(flag);
+        
+        int str_length = expression.size();
+        Stack bracket_stack(str_length);        
+        
+        bool flag = true;
+        for(int i = 0; i < str_length; ++i) {
+            if(is_open_bracket(expression[i])) {
+                bracket_stack.push(expression[i]);
+            }
+            else {
+                if(bracket_stack.pop() != return_couple_bracket(expression[i])) {
+                    flag = false;
+                    break;
+                }
+                bracket_stack.pop();
+            }
+        }
+        result.push_back(flag);
     }
-
-	for(int i = 0; i < t; ++i) {
-		if(result[i]) cout << "YES" << endl;
-		else cout << "NO" << endl;
-	}
+    
+    for(int i = 0; i < result.size(); ++i) {
+        if(result[i])
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
 
     return 0;
 }
+
